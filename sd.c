e33df7a663443c9910967ac31de1a9ddee86e79f
RafiTheGoat <40672500+RafiTheGoat@users.noreply.github.com>
2018-07-30 20:30:39 +0530

    Add files via upload

---
+++ sd.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	int i,j,c=0;// c will take garbage value if not initialized with zero
+
+	
+	char a[100], b[100];
+	char c[100];
+
+	printf("enter string: ");
+	gets(a);
+	printf("enter string: ");
+	gets(b);
+
+
+
+	/*for(i=0; i<=strlen(a); i++  ) // i is zero and will run less than or equal to the string
+	{
+		if(a[i]==' ' || a[i]=='\0') // a takes value of like a[o] a[1] a[2]
+		{
+			c++;
+		}
+	}*/
+	for(i=0; i<=strlen(a); i++)
+	{
+		if(a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u'){ // 
+			printf("\n %c %d - > (vowel) ", a[i], i);
+		}
+		printf("\n %c %d", a[i], i);// strcmp inside printf works
+
+	}
+	printf("\n difference is: %d \n",strcmp(a,b));// strcmp inside printf works
+	printf("\n concatenated: %s \n", strcat(a,b));
+	strcpy(c,strcat(a,b));
+	printf(" stored in c: %s ",c );
+
+	//printf("\nnumber of words: %d", c);
+	//printf("\n");
+	//printf("\n %c", a[2]);
+	//printf("\n");
+	//printf("")
+}
